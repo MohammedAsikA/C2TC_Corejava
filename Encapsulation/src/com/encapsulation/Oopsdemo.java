91bcf75fc77f57fdada9c3d3ff4d107c723f875e
ELCOT <ELCOT@Lenovo>
2024-07-26 16:20:46 -0700

    oopdemo

--- Encapsulation/src/com/encapsulation/Oopsdemo.java
+++ Encapsulation/src/com/encapsulation/Oopsdemo.java
@@ -1,5 +1,34 @@
 package com.encapsulation;
 
 public class Oopsdemo {
-
+	private int serialNum;
+	private String name;
+	private int age;
+	
+    public int getSerialNum() {
+    	return serialNum;
+    	
+    }
+    public void setSerialNUm(int serialNum) {
+    	this.serialNum=serialNum;
+    	
+    }
+    public String getName() {
+    	return name;
+    	
+    }
+    public void setName(String name) {
+    	this.name=name;
+    }
+    public int getAge() {
+    	return age;
+    }
+    public void setAge(int age) {
+    	this.age=age;
+    }
+	@Override
+	public String toString() {
+		return "Oopsdemo [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+    
 }
